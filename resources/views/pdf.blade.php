e1f559080c5cf6905e6ba4a302812a98ea412b7e
Ernesto Liberio Vera <sato@eliberio.domain.name>
2020-12-20 10:14:35 -0500

    fix to pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,22 +1,31 @@
-<h1>{{$title}}</h1>
-
-<b style="color: green">PRESENTANDO PDF</b>
 
 
-<table style="width: 100%">
-    <tr>
-        <td style="background: #1c4b30;color: white">NOMBRE</td>
-        <td style="background: #1c4b30;color: white">EMAIL</td>
-        <td style="background: #1c4b30;color: white">FECHA CREACION</td>
-    </tr>
+@php $datas=$users->chunk(20); @endphp
 
-    @foreach($users as $user)
+
+@foreach($datas as $keyD=>$data)
+    <h1>{{$title}}</h1>
+
+    <b style="color: green">PRESENTANDO PDF</b>
+    <table style="width: 100%">
         <tr>
-            <td>{{$user->name}}</td>
-            <td>{{$user->email}}</td>
-            <td>{{$user->created_at}}</td>
+            <td style="background: #1c4b30;color: white">FOTO</td>
+            <td style="background: #1c4b30;color: white">NOMBRE</td>
+            <td style="background: #1c4b30;color: white">EMAIL</td>
+            <td style="background: #1c4b30;color: white">FECHA CREACION</td>
         </tr>
-    @endforeach
 
+        @foreach($data as $user)
+            <tr>
+                <td> <img width="45px" class="hover:opacity-75" src="{{QData::getImageFromUser($user)}}"></td>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->created_at}}</td>
+            </tr>
+        @endforeach
+    </table>
+    @if(($keyD+1)<$datas->count())
+        <div style="page-break-after:always; clear:both"></div>
+    @endif
 
-</table>
+@endforeach
